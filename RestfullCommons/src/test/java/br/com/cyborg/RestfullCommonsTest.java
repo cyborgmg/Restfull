231a5b21c2e0cacbd39c04c4784adeb99d7a6a47
Unknown <cyborgmg@gmail.com>
2017-12-17 19:59:12 -0200

    17-dez-2017
    lib

---
+++ RestfullCommons/src/test/java/br/com/cyborg/RestfullCommonsTest.java
@@ -0,0 +1,38 @@
+package br.com.cyborg;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RestfullCommonsTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RestfullCommonsTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RestfullCommonsTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testRestfullCommons()
+    {
+        assertTrue( true );
+    }
+}
